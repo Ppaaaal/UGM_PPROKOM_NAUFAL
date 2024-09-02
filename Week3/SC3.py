b6e587cd1080dfe65101754c91f191ec930e0bd2
Ppaaaal <156084445+Ppaaaal@users.noreply.github.com>
2024-09-02 14:33:42 +0700

    Update SC3.py

--- Week3/SC3.py
+++ Week3/SC3.py
@@ -1,6 +1,7 @@
-m =['Alice','ants','Bob','badgers','Carol','cats']
-m.sort()
-print (m)
-spam = ['a','z','A','Z']
-spam.sort(key=str.lower)
-print (spam)
+from operator import *
+a = 1
+b = 5.0
+print('a = {}'.format(a))
+print('b = {}'.format(b))
+for func in (lt, le, eq, ne, ge, gt):
+  print ('{}(a, b): {}'.format(func.__name__, func(a, b)))
