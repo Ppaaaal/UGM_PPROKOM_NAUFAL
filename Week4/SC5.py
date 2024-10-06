c3da12d8ff80d2bb0aefff0229aefb565bba268b
Ppaaaal <156084445+Ppaaaal@users.noreply.github.com>
2024-10-06 20:01:52 +0700

    Create SC5.py

---
+++ Week4/SC5.py
@@ -0,0 +1,36 @@
+Siswa1 = str(input("Masukkan nama siswa: "))
+Siswa2 = str(input("Masukkan nama siswa: "))
+Siswa3 = str(input("Masukkan nama siswa: "))
+Siswa4 = str(input("Masukkan nama siswa: "))
+Siswa5 = str(input("Masukkan nama siswa: "))
+
+Nilai1 = int(input("Masukkan nilai siswa: "))
+Nilai2 = int(input("Masukkan nilai siswa: "))
+Nilai3 = int(input("Masukkan nilai siswa: "))
+Nilai4 = int(input("Masukkan nilai siswa: "))
+Nilai5 = int(input("Masukkan nilai siswa: "))
+
+if(Nilai1 >= 70) :
+  print("Siswa", Siswa1, "Lulus")
+else :
+  print("Siswa", Siswa1, "Tidak Lulus")
+
+if(Nilai2 >= 70) :
+  print("Siswa", Siswa2, "Lulus")
+else :
+  print("Siswa", Siswa2, "Tidak Lulus")
+
+if(Nilai3 >= 70) :
+  print("Siswa", Siswa3, "Lulus")
+else :
+  print("Siswa", Siswa3, "Tidak Lulus")
+
+if(Nilai4 >= 70) :
+  print("Siswa", Siswa4, "Lulus")
+else :
+  print("Siswa", Siswa4, "Tidak Lulus")
+
+if(Nilai5 >= 70) :
+  print("Siswa", Siswa5, "Lulus")
+else :
+  print("Siswa", Siswa5, "Tidak Lulus")
